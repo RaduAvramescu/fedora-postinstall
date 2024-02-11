ea3ed39f4258c7b8551cd524f6ea80366353902d
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2024-02-11 20:22:11 +0200

    Move setting up flatpak to separate flatpak file

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -49,44 +49,6 @@ function install_nvidia_drivers() {
     sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 }
 
-function add_flathub_repo() {
-    echo -ne "
--------------------------------------------------------------------------
-                    Adding Flathub repo
--------------------------------------------------------------------------
-"
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak remote-modify --enable flathub
-    flatpak update -y
-}
-
-function install_flatpaks() {
-    echo -ne "
--------------------------------------------------------------------------
-                    Installing $1 flatpaks
--------------------------------------------------------------------------
-"
-
-    cat "$2" | while read line
-    do
-        if flatpak list | grep -q "${line}"; then
-            echo "${line} is already installed"
-        else
-            flatpak install flathub -y --noninteractive "${line}"
-        fi
-    done
-}
-
-function handle_gaming_flatpaks() {
-    read -p "Do you want to install gaming flatpaks? (y/N) " answer
-
-    case $answer in 
-        y ) install_flatpaks "gaming" "gaming-flatpaks.txt";;
-        N ) ;;
-        * ) ;;
-    esac
-}
-
 function install_terminal() {
     echo -ne "
 -------------------------------------------------------------------------
@@ -168,9 +130,7 @@ if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
     install_nvidia_drivers
 fi
 
-add_flathub_repo
-install_flatpaks "generic" "generic-flatpaks.txt"
-handle_gaming_flatpaks
+./flatpak-setup.sh
 install_terminal
 install_tmux
 install_fonts
