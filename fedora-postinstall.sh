87eedef36f234d29fdcb6b10792654b3b568acbf
Radu <contact@raduavramescu.com>
2024-02-11 16:37:04 +0200

    Update setup_git function

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -58,7 +58,7 @@ function setup_git() {
     read name
     git config --global --unset user.name
     git config --global user.name ${name}
-    echo "Email Address:"
+    echo "Email Address: "
     read email
     git config --global --unset user.email
     git config --global user.email ${email}
