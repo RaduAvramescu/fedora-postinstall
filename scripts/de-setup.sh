537f5a9dabb5259b0b4d3d20810108d5ce67f76d
Radu <contact@raduavramescu.com>
2024-07-21 14:43:32 +0300

    Remove Hyprland post install config

--- scripts/de-setup.sh
+++ scripts/de-setup.sh
@@ -43,13 +43,13 @@ function handle_gnome_settings() {
 }
 
 function setup_favorite_apps() {
-    favorite_apps="$(getFavoriteApps)"
+    favorite_apps="$(get_favorite_apps)"
     favorite_apps="[${favorite_apps:2:${#favorite_apps}}]"
 
     gsettings set org.gnome.shell favorite-apps "$favorite_apps"
 }
 
-getFavoriteApps() {
+get_favorite_apps() {
     cat "../data/favorite-apps.txt" | while read line
     do
         echo -n ", '${line}'"
@@ -69,117 +69,15 @@ function handle_kde_settings() {
     kscreen-doctor output.1.vrrpolicy.automatic
 }
 
-function handle_hyprland_settings() (
-    echo -ne "
--------------------------------------------------------------------------
-                    Handling Hyprland settings
--------------------------------------------------------------------------
-"
-    # Setup themes
-    mkdir -p ~/.icons
-    mkdir -p ~/.config/gtk-4.0
+case $XDG_SESSION_DESKTOP in
+    gnome | GNOME)
+        handle_gnome_settings
+        ;;
 
-    cd ../themes
+    kde | KDE)
+        handle_kde_settings
+        ;;
 
-    # Copy cursor theme
-    tar -xvf Simp1e-Tokyo-Night.tar.xz
-    mkdir -p ~/.icons/Simp1e-Tokyo-Night
-    sudo mkdir -p /usr/share/icons/Simp1e-Tokyo-Night
-    cp -rf Simp1e-Tokyo-Night ~/.icons/Simp1e-Tokyo-Night
-    sudo cp -rf Simp1e-Tokyo-Night /usr/share/icons/Simp1e-Tokyo-Night
-
-    # Copy general theme
-    unzip 01-Tokyonight-Dark-B.zip -d 01-Tokyonight-Dark-B
-    cd 01-Tokyonight-Dark-B/
-    mkdir -p ~/.themes/Tokyonight-Dark-B
-    sudo mkdir -p /usr/share/themes/Tokyonight-Dark-B
-    cp -rf Tokyonight-Dark-B ~/.themes/Tokyonight-Dark-B
-    sudo cp -rf Tokyonight-Dark-B /usr/share/themes/Tokyonight-Dark-B
-
-    # Copy wallpaper
-    cd ../wallpapers
-    cp tokyocity.png ~/Pictures
-
-    # Copy icon theme
-    cd ~/Repos
-    sudo rm -rf Tokyo-Night-GTK-Theme
-    git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
-    cd Tokyo-Night-GTK-Theme
-    mkdir -p ~/.icons/Tokyonight-Dark
-    sudo mkdir -p /usr/share/icons/Tokyonight-Dark
-    cp -rf Tokyonight-Dark ~/.icons/Tokyonight-Dark
-    sudo cp -rf Tokyonight-Dark /usr/share/icons/Tokyonight-Dark
-    cd ..
-    sudo rm -rf Tokyo-Night-GTK-Theme
-    
-    ln -s ~/.themes/Tokyonight-Dark-B/gtk-4.0/assets ~/.config/gtk-4.0/assets
-    ln -s ~/.themes/Tokyonight-Dark-B/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
-    ln -s ~/.themes/Tokyonight-Dark-B/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
-
-    sudo flatpak override --filesystem=~/.themes
-    sudo flatpak override --filesystem=~/.icons
-    flatpak override --user --env=XCURSOR_PATH=~/.icons
-    flatpak override --user --filesystem=~/.themes:ro --filesystem=~/.icons:ro
-    flatpak override --user --filesystem=~/.icons:ro --filesystem=/usr/share/icons/:ro
-    flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
-    flatpak override --user --filesystem=xdg-config/gtk-4.0:ro
-    sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro
-
-    gsettings set org.gnome.desktop.interface icon-theme 'Tokyonight-Dark'
-    gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Dark-B'
-    gsettings set org.gnome.desktop.interface cursor-theme 'Simp1e-Tokyo-Night'
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font, 11'
-)
-
-function setup_sddm() (
-    sudo -s
-
-    # Enable sddm to run on boot and start immediately
-    systemctl set-default graphical.target
-    systemctl enable sddm
-    systemctl start sddm
-
-    # Install theme dependencies
-    dnf install -y qt6-qt5compat qt6-qtdeclarative qt6-qtsvg
-
-    # Create all sddm folders if they don't exist
-    mkdir -p /usr/share/sddm/themes
-    mkdir -p /etc/sddm.conf.d/
-
-    # Clone theme
-    git clone https://github.com/RaduAvramescu/sddm-tokyo-night-theme /usr/share/sddm/themes/sddm-tokyo-night-theme
-
-    # Set sddm theme
-    echo -e "[Theme]\nCurrent=sddm-tokyo-night-theme" | tee /etc/sddm.conf.d/10-theme.conf
-)
-
-function install_hyprland() {
-    echo -ne "
--------------------------------------------------------------------------
-                    Installing Hyprland
--------------------------------------------------------------------------
-"
-    # Install Hyprland specific packages
-    cat "../data/hyprland-rpms.txt" | while read line
-    do
-        sudo dnf install -y "${line}"
-    done
-
-    # Update Hyprland packages from copr
-    sudo dnf copr enable solopasha/hyprland
-    sudo dnf upgrade -y --refresh
-
-    # Install swaync from copr
-    sudo dnf copr enable erikreider/SwayNotificationCenter
-    sudo dnf install -y SwayNotificationCenter
-
-    # Remove unnecessary packages installed as dependencies
-    sudo dnf remove -y kitty wofi
-
-    xdg-user-dirs-update
-    setup_sddm
-    handle_hyprland_settings
-}
-
-handle_hyprland_settings
+    *)
+        echo "Unknown DE!"
+esac
