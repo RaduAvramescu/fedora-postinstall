2530b629b6ca03df766d5741d253c1a4a2c7943b
Radu <contact@raduavramescu.com>
2024-08-20 20:29:25 +0300

    Remove shell change

--- scripts/terminal-setup.sh
+++ scripts/terminal-setup.sh
@@ -11,15 +11,6 @@ function install_terminal() {
     # curl https://codeload.github.com/ilancosman/tide/tar.gz/v6 | tar -xzC $_tide_tmp_dir
     # command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
     # fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
-
-    # Point to /bin/zsh for example, if using zsh
-    if [ $(which chsh) ]; then
-        chsh -s /bin/fish
-    elif [ $(which lchsh) ]; then
-        sudo lchsh $USER
-    else
-        echo "Failed to change shell!"
-    fi
 }
 
 function install_tmux() {
