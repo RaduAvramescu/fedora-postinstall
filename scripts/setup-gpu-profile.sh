88b3bceb0c3bc5bffde5c4157e044e1ecd867c60
Radu <contact@raduavramescu.com>
2024-08-31 17:47:55 +0300

    Fix gpu profile fan curve not applying on boot; fix gpu profile script SELinux label; update gpu profile systemd service to oneshot

--- scripts/setup-gpu-profile.sh
+++ scripts/setup-gpu-profile.sh
@@ -1,10 +1,15 @@
 # Copy GPU profile script
-sudo mkdir -p /root/.local/bin
-sudo cp ./gpu-profile.sh /root/.local/bin/gpu-profile.sh
-sudo chmod +x /root/.local/bin/gpu-profile.sh
+sudo mkdir -p /usr/local/bin
+sudo cp ./gpu-profile.sh /usr/local/bin/gpu-profile.sh
+sudo chmod +x /usr/local/bin/gpu-profile.sh
 
 # Copy GPU profile service
 sudo cp ./gpu-profile.service /etc/systemd/system/gpu-profile.service
 
 # Enable GPU profile script immediately
 systemctl enable --now gpu-profile.service
+
+# Fix SELinux label of script file
+if [ $(which restorecon) ]; then
+    sudo restorecon -RFv /usr/local/bin/
+fi
