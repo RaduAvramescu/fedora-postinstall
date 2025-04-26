d011790e433fb09f4248f5736fea9bdc42b2a18f
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2025-04-26 16:37:25 +0300

    chore: remove goxlr setup

--- scripts/goxlr-setup.sh
+++
@@ -1,5 +0,0 @@
-# Copy GoXLR Utility service
-sudo cp ./goxlr-utility.service /etc/systemd/user/goxlr-utility.service
-
-# Enable service immediately
-systemctl enable --user --now goxlr-utility.service
