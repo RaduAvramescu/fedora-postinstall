3c3b85036f2ff39ca0ac63bdf61e433f2d755ec5
Radu Avramescu <contact@raduavramescu.com>
2025-09-27 21:39:53 +0300

    refactor: move git setup to a separate script

--- scripts/fedora-workstation-postinstall.sh
+++ scripts/fedora-workstation-postinstall.sh
@@ -1,40 +1,6 @@
 #!/usr/bin/env bash
 # Fedora Workstation postinstall script
 
-function setup_git() {
-    echo -ne "
--------------------------------------------------------------------------
-                    Setting up git
--------------------------------------------------------------------------
-"
-    echo "Name: "
-    read name
-    git config --global --unset user.name
-    git config --global user.name ${name}
-    echo "Email Address: "
-    read email
-    git config --global --unset user.email
-    git config --global user.email ${email}
-    git config --global init.defaultBranch main
-
-    # Setup commit signing
-    git config --global "gpg.ssh.defaultKeyCommand" "ssh-add -L"
-    git config --global gpg.format ssh
-    git config --global commit.gpgsign true
-    git config --global format.signoff true
-}
-
-function prompt_git() {
-    if [ $(which git) ]; then
-        read -p "Do you want to setup git? (y/N) " answer
-
-        case $answer in 
-            y ) setup_git;;
-            N ) ;;
-            * ) ;;
-        esac
-    fi
-}
 
 function add_rpm_fusion_repos() {
     echo -ne "
@@ -87,7 +53,11 @@ sudo dnf upgrade -y --refresh
 
 # Make folder where all repos are stored
 mkdir -p ~/Repos
-prompt_git
+
+# Setup git
+chmod u+x ./generic/git-setup.sh
+./generic/git-setup.sh
+
 prompt_rpm_fusion_repos
 
 # Handle GPU setup
