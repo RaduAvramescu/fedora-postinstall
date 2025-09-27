f7fdde72eec57082e0161cf5931becb1658e1e9b
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2025-09-27 21:35:56 +0300

    refactor: merge generic and gaming flatpaks while removing Bottles

--- scripts/generic/flatpak-setup.sh
+++ scripts/generic/flatpak-setup.sh
@@ -4,7 +4,4 @@ chmod u+x ./flathub-setup.sh
 ./flathub-setup.sh
 
 chmod u+x ./flatpaks-install.sh
-./flatpaks-install.sh "generic" "../data/generic-flatpaks.txt"
-
-chmod u+x ./gaming-flatpaks.sh
-./gaming-flatpaks.sh
+./flatpaks-install.sh "flatpaks" "../data/flatpaks.txt"
