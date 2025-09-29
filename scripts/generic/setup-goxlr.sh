975d7196e75d71fc5b7fb14b001d5be21d336115
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2025-09-29 22:42:37 +0300

    feat: implement distrobox for goxlr utility

--- scripts/generic/setup-goxlr.sh
+++ scripts/generic/setup-goxlr.sh
@@ -1,3 +1,6 @@
+# Create GoXLR Utility distrobox
+distrobox-assemble create --file ./distrobox/goxlr-utility/distrobox.ini
+
 # Copy GoXLR Utility service
 sudo cp ./systemd/goxlr-utility.service /etc/systemd/user/goxlr-utility.service
 
