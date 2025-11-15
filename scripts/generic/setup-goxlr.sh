c0deca6f2ba0754b0084a1326255e429594ea52b
Radu Avramescu <contact@raduavramescu.com>
2025-11-15 13:47:49 +0200

    chore: update goxlr setup steps

--- scripts/generic/setup-goxlr.sh
+++ scripts/generic/setup-goxlr.sh
@@ -1,8 +1,3 @@
 # Create GoXLR Utility distrobox
 distrobox-assemble create --file ./distrobox/goxlr-utility/distrobox.ini
 
-# Copy GoXLR Utility service
-sudo cp ./systemd/goxlr-utility.service /etc/systemd/user/goxlr-utility.service
-
-# Enable service immediately
-systemctl enable --user --now goxlr-utility.service
